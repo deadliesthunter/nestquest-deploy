fa8f6362514ec88b0b3829cb777c32b7e1ab2bd6
deadliesthunter <subedimanish55@gmail.com>
2025-05-14 22:09:28 +0545

    home

--- server/urls.py
+++ server/urls.py
@@ -19,6 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Hello, this is the homepage!")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
